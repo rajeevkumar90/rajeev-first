08d496899c86d9e8a1102ed28a9ebd55c9e9a6af
Rajeev <rajeevkumargreat@gmail.com>
2021-11-06 11:28:48 +0530

    Putting it together and added price click.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ collapsibles.forEach(item => {
     this.classList.toggle('collapsible--expanded');
   });
 });
+
+const domains = document.querySelectorAll('.price');
+domains.forEach(item => {
+  item.addEventListener('click', function () {
+    domains.forEach(item => item.classList.remove('badge--secondary'));
+    item.classList.add('badge--secondary');
+    const selected = item.innerHTML;
+    console.log(selected);
+  });
+});
